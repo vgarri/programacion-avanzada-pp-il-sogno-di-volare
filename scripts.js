53afacef3fb24893ca4f3221e422a8c8faf6e814
Victor Garritano <victor@getupgraded.com>
2024-10-20 20:49:39 +0200

    Terminado

--- scripts.js
+++ scripts.js
@@ -32,35 +32,55 @@ const militaryHelicopter = [
 
 class Gallery {
     constructor(civilImages, militaryImages) {
+        this.civilImages = civilImages;
+        this.militaryImages = militaryImages;
     }
     
     getRandomCivil() {
+        return this.civilImages[Math.floor(Math.random() * 4)];
     }
     
     getRandomMilitary() {
+        return this.militaryImages[Math.floor(Math.random() * 4)];
     }
     
     getAll() {
+        return [...this.civilImages, ...this.militaryImages];
     }
 }
 
 class Painter {
-    constructor() {
+    constructor(gallery) {
+        this.createGallery();
     }
 
-    createGallery() {
+    createGallery(gallery) {
+        this.gallery = document.createElement("section");
+        document.body.appendChild(this.gallery);
     }
 
     createImageTag(imageUrl) {
+        return `
+        <picture>
+            <img src="${imageUrl}" />
+        </picture>
+        `
     }
 
     paintSingleImage(imageUrl) {
+        
+        this.gallery.innerHTML += this.createImageTag(imageUrl);
     }
 
     paintMultipleImages(arrayOfImages) {
+        for (let i = 0; i < arrayOfImages.length; i++) {
+            this.gallery.innerHTML += this.createImageTag(arrayOfImages[i]);
+            
+        }
     }
 }
 
 const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
 const helicopters = new Gallery(civilHelicopters, militaryHelicopter);
 const painter = new Painter();
+
